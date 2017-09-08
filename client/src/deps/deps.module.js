8b04c26886c408a617f9d51b3147f64493f168e3
Tomek Zak <tz@impulse.pl>
2017-09-09 00:12:11 +0200

    Add angular material

--- client/src/deps/deps.module.js
+++ client/src/deps/deps.module.js
@@ -1,12 +1,20 @@
 import angular from 'angular';
 
 import uirouter from 'angular-ui-router';
+import angularAria from 'angular-aria';
+import angularAnimate from 'angular-animate';
+import angularMessages from 'angular-messages';
+import angularMaterial from 'angular-material';
 
 import depsConfig from './deps.config';
 
 export const deps = 'app.deps';
 angular
   .module(deps, [
-    uirouter
+    uirouter,
+    angularAria,
+    angularAnimate,
+    angularMessages,
+    angularMaterial
   ])
   .config(depsConfig);
