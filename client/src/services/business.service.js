4b9e426578be4637630f4fee582e6ba59df27447
Tomek Zak <tz@impulse.pl>
2017-09-09 01:40:40 +0200

    Add business service

--- client/src/services/business.service.js
+++ client/src/services/business.service.js
@@ -1,5 +1,4 @@
 export class BusinessService {
-<<<<<<< HEAD
   constructor($http, $log) {
     'ngInject';
     this.$http = $http;
@@ -9,13 +8,3 @@ export class BusinessService {
     return this.$http.get('localhost:5000/business/all').then(response => this.$log('first fetch!', response.data));
   }
 }
-=======
-  constructor($http) {
-    'ngInject';
-    this.$http = $http;
-  }
-  getAllBusinesses() {
-    return this.$http.get('/api/allBusinesses').then(response => response.data);
-  }
-}
->>>>>>> 129ff63a7b8dd673001abecfa6ed97bd9662773c
